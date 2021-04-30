28016851e891ce3b1da6d22efce514104dc2069f
Yuri de Oliveira Dantas <iruy3343@gmail.com>
2021-04-30 19:41:18 -0300

    Create the game functions

--- jogo-da-velha/scripts/interface.js
+++ jogo-da-velha/scripts/interface.js
@@ -1,29 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
-
     let squares = document.querySelectorAll(".square");
-
     squares.forEach((square) => {
         square.addEventListener('click', handleClick);
     })
-
 })
 
 function handleClick(event) {
     let square = event.target;
     let position = square.id;
 
-    handleMove(position);
-    updateSquares();
-}
+    if (handleMove(position)) {
+        setTimeout(() => {
+            alert('O jogo acabou - O vencedor foi ' + playerTime);
+        }, 10);
+    };
+    updateSquare(position);
+};
 
-function updateSquares(){
+function updateSquare(position) {
+    let square = document.getElementById(position.toString())
+    let symbol = board[position];
+    square.innerHTML = `<div class='${symbol}'></div>`
+};
+
+function updateSquares() {
     let squares = document.querySelectorAll(".square");
-
     squares.forEach((square) => {
         let position = square.id;
         let symbol = board[position];
 
-        if(symbol != ''){
+        if (symbol != '') {
             square.innerHTML = `<div class='${symbol}'></div>`
         }
     })
