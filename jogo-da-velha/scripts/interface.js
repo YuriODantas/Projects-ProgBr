8ee7cd127d6bd0bfc7c8af3340bf235b5ea43697
Yuri de Oliveira Dantas <iruy3343@gmail.com>
2021-05-01 16:39:39 -0300

    Create the updatePoints function

--- jogo-da-velha/scripts/interface.js
+++ jogo-da-velha/scripts/interface.js
@@ -5,13 +5,27 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 })
 
+let players = document.querySelector('.main-points');
+let player1 = 0;
+let player2 = 0;
+
+function updatePoints() {
+    if (playerTime == 0) {
+        player1++
+    } else if (playerTime == 1) {
+        player2++
+    }
+
+    players.innerHTML = `<h2 class="player1">Player1: ${player1}</h2>
+    <h2 class="player2">Player2: ${player2}</h2>`
+}
 function handleClick(event) {
     let square = event.target;
     let position = square.id;
 
     if (handleMove(position)) {
         setTimeout(() => {
-            alert('O jogo acabou - O vencedor foi ' + playerTime);
+            updatePoints();
         }, 10);
     };
     updateSquare(position);
@@ -22,15 +36,3 @@ function updateSquare(position) {
     let symbol = board[position];
     square.innerHTML = `<div class='${symbol}'></div>`
 };
-
-function updateSquares() {
-    let squares = document.querySelectorAll(".square");
-    squares.forEach((square) => {
-        let position = square.id;
-        let symbol = board[position];
-
-        if (symbol != '') {
-            square.innerHTML = `<div class='${symbol}'></div>`
-        }
-    })
-}
