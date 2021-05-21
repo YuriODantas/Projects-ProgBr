f58a77247fa23bb396940da9b56fc5359be016d9
Yuri de Oliveira Dantas <iruy3343@gmail.com>
2021-05-21 10:58:11 -0300

    Separates more components

--- to-do-list/src/components/List.js
+++ to-do-list/src/components/List.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import Card from './Card';
+import ListItem from './ListItem';
 
-function DoneImg(props) {
-    if (props.done) {
-        return (<img src="./assets/on.png" alt="done" />)
-    } else {
-        return (<img src="./assets/off.png" alt="undone" />)
-    }
-}
+
 
 function List(props) {
     return (
         <ul>
-            {props.items.map(item => <li key={item.id}>
-                <Card className={item.done ? "done item" : "item"}>
-                    {item.text}
-                    <div>
-                        <button onClick={() => { props.onDone(item) }}><DoneImg done={item.done} /></button>
-                        <button onClick={() => { props.onItemDeleted(item) }}><img src="./assets/bin.svg" alt="delete" /></button>
-                    </div>
-                </Card>
-            </li>)}
+            {props.items.map(item => <ListItem key={item.id} item={item} onDone={props.onDone} onItemDeleted={props.onItemDeleted}></ListItem>)}
         </ul>
     )
 }
---
+++ to-do-list/src/components/ListItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Card from './Card';
+
+function DoneImg(props) {
+    if (props.done) {
+        return (<img src="./assets/on.png" alt="done" />)
+    } else {
+        return (<img src="./assets/off.png" alt="undone" />)
+    }
+}
+
+function ListItem(props) {
+    return (
+        <li>
+            <Card className={props.item.done ? "done item" : "item"}>
+                {props.item.text}
+                <div>
+                    <button onClick={() => { props.onDone(props.item) }}><DoneImg done={props.item.done} /></button>
+                    <button onClick={() => { props.onItemDeleted(props.item) }}><img src="./assets/bin.svg" alt="delete" /></button>
+                </div>
+            </Card>
+        </li>)
+}
+
+export default ListItem;
